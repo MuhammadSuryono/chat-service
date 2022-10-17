38dea33b77a5373ea2c3efe1b9bf4930adfb1cae
Muhammad Suryono <msuryono0@gmail.com>
2022-10-17 10:10:07 +0700

    Update first message can't load

--- repository/message/message.go
+++ repository/message/message.go
@@ -3,8 +3,6 @@ package message
 import (
 	"chat-service/config/database"
 	"chat-service/models/tables"
-	"chat-service/system"
-	"fmt"
 )
 
 type message struct {
@@ -19,19 +17,20 @@ func (g message) LastMessageGroup(groupId int64) (msg tables.Message) {
 
 func (g message) MessageGroup(groupId int64, limit int, page int, lastDate string) (msg []tables.Message, total int64) {
 	offset := (page * limit) - limit
-	fmt.Println("Offset", offset, page, limit)
-	query := database.Connection.Where("group_id = ?", groupId).Debug().Offset(offset).Limit(limit).Order("created_at ASC")
-	if lastDate == "" {
-		lastDate = system.TimeNowString()
-	}
-	if lastDate != "" {
-		query = query.Where("created_at <= ?", lastDate)
-	} else {
-		query = query.Where("DATE(created_at) <= ?", system.TimeNowString())
-	}
-
-	query.Find(&msg)
-	query.Count(&total)
+	//fmt.Println("Offset", offset, page, limit)
+	//query := database.Connection.Where("group_id = ?", groupId).Debug().Offset(offset).Limit(limit).Order("created_at ASC")
+	//if lastDate == "" {
+	//	lastDate = system.TimeNowString()
+	//}
+	//if lastDate != "" {
+	//	query = query.Where("created_at <= ?", lastDate)
+	//} else {
+	//	query = query.Where("DATE(created_at) <= ?", system.TimeNowString())
+	//}
+	//
+	//query.Find(&msg)
+	//query.Count(&total)
+	database.Connection.Where("group_id = ?", groupId).Debug().Limit(limit).Offset(offset).Order("created_at ASC").Find(&msg)
 
 	//for i, j := 0, len(msg)-1; i < j; i, j = i+1, j-1 {
 	//	msg[i], msg[j] = msg[j], msg[i]
